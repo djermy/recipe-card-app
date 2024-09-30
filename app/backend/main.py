1c4fd72334910abf14b9cbf011290e47d888a150
djermy <danjermy@protonmail.com>
2024-09-30 15:20:45 +0200

    tested put endpoint, added run_dev.sh, added requirements.txt, added Dockerfile/.dockerignore

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Recipe(BaseModel):
+    name: str
+    ingredients: str
+    prep_time: int | float
+    beginner_friendly: bool
 
 @app.get("/")
 def read_root():
@@ -9,3 +16,12 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q" : q}
+
+@app.put("/recipes/{recipe_id}")
+def update_recipe(recipe_id: int, recipe: Recipe):
+    return {
+        "recipe_name": recipe.name,
+        "ingredients": recipe.ingredients,
+        "prep_time": recipe.prep_time,
+        "beginner_friendly": recipe.beginner_friendly
+    }
