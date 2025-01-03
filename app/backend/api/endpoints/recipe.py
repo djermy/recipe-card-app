d3534b0ff5fe3b4fd42009afb43b6df52546bb83
djermy <danjermy@protonmail.com>
2025-01-03 07:12:54 +0100

    changed endpoints to be async, removed unnecessary dependencies.py and code

--- app/backend/api/endpoints/recipe.py
+++ app/backend/api/endpoints/recipe.py
@@ -1,14 +1,12 @@
-from fastapi import APIRouter, Depends, HTTPException
-from ...dependencies import get_token_header
+from fastapi import APIRouter
 from backend.models.recipe import Recipe
 
 router = APIRouter(
     prefix="/recipe",
     tags=["recipe"],
-    dependencies=[Depends(get_token_header)],
     responses={404: {"description": "Not found"}},
 )
 
 @router.post("/create")
-def create_recipe(recipe: Recipe):
+async def create_recipe(recipe: Recipe):
     return recipe
--- app/backend/main.py
+++ app/backend/main.py
@@ -1,13 +1,10 @@
-from fastapi import Depends, FastAPI
-from .dependencies import get_query_token, get_token_header
+from fastapi import FastAPI
 from .api.endpoints import recipe
 
-app = FastAPI(
-    dependencies=[Depends(get_query_token)]
-)
+app = FastAPI()
 
 app.include_router(recipe.router)
 
 @app.get("/")
-def root():
+async def root():
     return {"hello": "world!"}
