c3c925c16e02e242947e3be44c8a9679e29fea6c
djermy <danjermy@protonmail.com>
2025-01-14 22:56:11 +0100

    updated recipe model, created frontend files, started html files for create recipe and index

--- app/backend/models/recipe.py
+++ app/backend/models/recipe.py
@@ -3,7 +3,7 @@ from sqlmodel import Field, SQLModel
 class Recipe(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True)
-    cook_time_hrs: float = Field(index=True)
+    cook_time_mins: int = Field(index=True)
     difficulty: str = Field(index=True, max_length=50) 
     rating: float | None = Field(default=0.0, index=True) 
     likes: int | None = Field(default=0, index=True)
